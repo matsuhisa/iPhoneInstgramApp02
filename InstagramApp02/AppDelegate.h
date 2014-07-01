3a725357e0f35a8fed51eb0ad90a0ad70c1bd09a
Hironobu Matsuhisa <matsuhisa@gmail.com>
2014-07-02 05:48:44 +0900

    Initial Commit

---
+++ InstagramApp02/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InstagramApp02
+//
+//  Created by 松久浩伸 on 2014/07/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstagramApp02/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstagramApp02
+//
+//  Created by 松久浩伸 on 2014/07/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
